9fd101e5b344ea81f65744883e6521886135f53c
Farhanaaz522 <37798788+Farhanaaz522@users.noreply.github.com>
2018-05-14 22:22:52 +0530

    Add files via upload

---
+++ assignment 3.2.R
@@ -0,0 +1,30 @@
+#assignment 3.2
+
+rownames(mtcars)= mtcars$X
+mtcars1= mtcars[,-1]
+View(mtcars1)
+vec1 = c(rownames(mtcars[1:15,])) 
+vec2 = c(rownames(mtcars[10:32,])) 
+vec1
+vec2
+#1. Obtain the elements of the union between two character vectors. 
+#vec1 = c(rownames(mtcars[1:15,])) 
+#vec2 = c(rownames(mtcars[10:32,])) 
+
+union(vec1,vec2)
+
+#2. Get those elements that are common to both vectors. 
+#vec1 = c(rownames(mtcars[1:15,])) 
+#vec2 = c(rownames(mtcars[10:32,])) 
+intersect(vec1,vec2)
+
+#3. Get the difference of the elements between two character vectors.
+#vec1 = c(rownames(mtcars[1:15,])) 
+#vec2 = c(rownames(mtcars[10:32,])) 
+
+diff_in_vec1= setdiff(vec1,vec2)
+diff_in_vec2= setdiff(vec2,vec1)
+diff_in_vec1
+diff_in_vec2
+diff_of_two= c(diff_in_vec2, diff_in_vec1)
+diff_of_two
